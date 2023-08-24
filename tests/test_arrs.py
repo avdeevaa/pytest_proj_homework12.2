4dc98771c0bf86df00fb9eff3fd9348e97b2e456
avdeevaa <tess-avdeeva@seznam.cz>
2023-08-24 13:07:57 +0200

    100% coverage - ok

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,10 +2,15 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2 #mis
+    assert arrs.get([1, 2, 3], 1, "test") == 2 #solved
     assert arrs.get([], 0, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 0, 2) == []
+    assert arrs.my_slice([1, 2, 3, 4], None, None) == [1, 2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], None, 2) == [1, 2]
+
+
